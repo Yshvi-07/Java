d67e5d8da47ce52e8ac6ac5740a62f4b539c2a7b
Yashasvi <princiyyv1302@gmail.com>
2024-06-28 00:14:41 +0530

    Create switch.java

---
+++ switch.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        switch(a){
+        case 1:System.out.println("Jan");
+        break;
+        case 2:System.out.println("Feb");
+        break;
+        case 3:System.out.println("mARCH");
+        break;
+        case 4:System.out.println("APRIL");
+        break;
+        default:System.out.println("Nhi hoga bhai");
+        }
+        
+    }
+}
