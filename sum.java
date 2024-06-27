7de6452ba92238b36c949cdda45594969190460f
Yashasvi <princiyyv1302@gmail.com>
2024-06-27 23:11:05 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	
+		Scanner n=new Scanner(System.in);
+		int name=n.nextInt();
+	
+		int m=n.nextInt();
+		System.out.println(m+name);
+		
+		    
+		
+	}
+}
