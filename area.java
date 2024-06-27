f72eaacd603d96b4c0bce990ce23ac0ffafa46b1
Yashasvi <princiyyv1302@gmail.com>
2024-06-27 23:24:04 +0530

    Create area.java

---
+++ area.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		Scanner sc=new Scanner(System.in);
+		int r=sc.nextInt();
+		double area=(3.14*r*r);
+		
+		double c=(2*3.14*r);
+		System.out.println(area);
+		System.out.println(c);
+		
+	}
+}
