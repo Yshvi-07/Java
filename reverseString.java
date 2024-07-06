1af5fe994a0a0b704e60c13b0789f485708e3df7
Yashasvi <princiyyv1302@gmail.com>
2024-07-06 21:50:59 +0530

    Create reverseString.java

---
+++ reverseString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		StringBuilder sb=new StringBuilder("Hello");
+		for(int i=sb.length()-1;i>=0;i--){
+		    System.out.println(sb.charAt(i));		}
+		
+	}
+}
